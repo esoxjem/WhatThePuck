6b44ee15283385f02782c304933ac2a4387e3613
Arun Sasidharan <arunsasidharan121@gmail.com>
2025-12-21 13:31:22 +0530

    Initial Commit

---
+++ WhatThePuck/WhatThePuckApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatThePuckApp.swift
+//  WhatThePuck
+//
+//  Created by Arun Sasidharan on 21/12/25.
+//
+
+import SwiftUI
+
+@main
+struct WhatThePuckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
